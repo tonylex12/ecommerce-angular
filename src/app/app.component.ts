d5cfba122523e009011508d61188ecb743b67e36
Tony HUerto <tonylex12@gmail.com>
2025-01-08 18:31:14 -0500

    Template added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+
+declare var $: any;
+declare function HOMEINIT([]): any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ecommerce';
+
+  constructor() {
+    setTimeout(() => {
+      HOMEINIT($);
+    }, 50);
+
+    $(window).on('load', function () {
+      $("#loading").fadeOut(500);
+    });
+  }
 }
